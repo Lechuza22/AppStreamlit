2c5beb1df125e0c96f87765fb37b79ac873b73bd
Jerónimo Martínez <110127604+Lechuza22@users.noreply.github.com>
2025-01-16 18:11:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,11 @@ import streamlit as st
 
 def main():
     st.title("Curso de streamlit jero gato")
+    st.header ("Encabezado")
+    st.subheader ("subencabezado)
+    st.text ("introduce texto)
+    nombre = "Jero"    
+    st.text(f"Hola {nombre}, esto es una prueba")
+    st.markdown ("### Esto es un Markdown") 
 if __name__== '__main__':
     main()
