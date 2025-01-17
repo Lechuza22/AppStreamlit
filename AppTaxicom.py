f7737c1fee7b43ae67042f3b022c0dbf1dfc1c7c
Jerónimo Martínez <110127604+Lechuza22@users.noreply.github.com>
2025-01-17 12:30:24 -0300

    Update AppTaxicom.py

--- AppTaxicom.py
+++ AppTaxicom.py
@@ -98,20 +98,20 @@ if menu_option == "Comparar marcas y modelos":
 
     # Graficar comparación
     if selected_variables:
-    st.subheader("Gráfico comparativo")
-    fig, ax = plt.subplots(figsize=(6, 4)) 
-
-    x = range(len(selected_variables))
-    ax.bar(x, data_model1.iloc[0], width=0.4, label=model1, align="center", color="blue")
-    ax.bar([i + 0.4 for i in x], data_model2.iloc[0], width=0.4, label=model2, align="center", color="orange")
-
-    ax.set_xticks([i + 0.2 for i in x])
-    ax.set_xticklabels(selected_variables, rotation=45, fontsize=8)  
-    ax.set_title("Comparación de modelos", fontsize=10)  
-    ax.legend(fontsize=8)  
-
-    plt.tight_layout()  
-    st.pyplot(fig, bbox_inches="tight")
+        st.subheader("Gráfico comparativo")
+        fig, ax = plt.subplots(figsize=(6, 4)) 
+    
+        x = range(len(selected_variables))
+        ax.bar(x, data_model1.iloc[0], width=0.4, label=model1, align="center", color="blue")
+        ax.bar([i + 0.4 for i in x], data_model2.iloc[0], width=0.4, label=model2, align="center", color="orange")
+    
+        ax.set_xticks([i + 0.2 for i in x])
+        ax.set_xticklabels(selected_variables, rotation=45, fontsize=8)  
+        ax.set_title("Comparación de modelos", fontsize=10)  
+        ax.legend(fontsize=8)  
+    
+        plt.tight_layout()  
+        st.pyplot(fig, bbox_inches="tight")
 
 elif menu_option == "Predicciones":
     st.header("Predicciones")
