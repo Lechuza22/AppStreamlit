56905e124ce8428e8dcf6d84d6df051b89c9a884
Jerónimo Martínez <110127604+Lechuza22@users.noreply.github.com>
2025-01-17 12:26:18 -0300

    Update AppTaxicom.py

--- AppTaxicom.py
+++ AppTaxicom.py
@@ -99,10 +99,10 @@ if menu_option == "Comparar marcas y modelos":
     # Graficar comparación
     if selected_variables:
         st.subheader("Gráfico comparativo")
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(6, 4))
 
         x = range(len(selected_variables))
-        ax.bar(x, data_model1.iloc[0], width=0.2, label=model1, align="center", color="blue")
+        ax.bar(x, data_model1.iloc[0], width=0.4, label=model1, align="center", color="blue")
         ax.bar([i + 0.4 for i in x], data_model2.iloc[0], width=0.4, label=model2, align="center", color="orange")
 
         ax.set_xticks([i + 0.2 for i in x])
